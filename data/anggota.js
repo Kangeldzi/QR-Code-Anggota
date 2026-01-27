06786bc28eb1e20ef68bd7e0f8a436d2232711e0
Kang L <143698732+Kangeldzi@users.noreply.github.com>
2026-01-27 10:06:20 +0700

    Create anggota.js

---
+++ data/anggota.js
@@ -0,0 +1,7 @@
+// Alternatif API URL (jika Google Apps Script susah)
+const CONFIG = {
+  // Pilih salah satu:
+  // API_URL: 'https://script.google.com/macros/s/XXXXX/exec', // Google Apps Script
+  API_URL: 'https://raw.githubusercontent.com/Kangeldzi/QR-Code-Anggota/main/data/anggota.json', // JSON file
+  DEFAULT_ID: 'NIA-001'
+};
